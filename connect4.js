2ccc52117d99ea68030c34b02be3430c62027008
Adrian <adrianbb21@gmail.com>
2023-04-04 03:19:57 -0400

    completed exercise

--- connect4.js
+++ connect4.js
@@ -1,9 +1,3 @@
-/** Connect Four
- *
- * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
- * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
- */
 
 const WIDTH = 7;
 const HEIGHT = 6;
